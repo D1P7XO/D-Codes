1fbd7bb30ac0cb00778b9a14f8ade74dab5ebf15
Diptoyree <diptoasdipto@gmail.com>
2021-12-19 00:17:17 +0600

    Update

--- vrSwitch.cs
+++ vrSwitch.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using Google.XR.Cardboard;
+using System;
 using System.Collections;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.XR.Management;
 
 public class vrSwitch : MonoBehaviour
 {
@@ -10,20 +12,57 @@ public class vrSwitch : MonoBehaviour
     {
         if (!VR)
         {
-            StartCoroutine(LoadDevice("None"));
+            ExitVR();
         }
         else
         {
-            StartCoroutine(LoadDevice("cardboard"));
+            EnterVR();
         }    
     }
-    IEnumerator LoadDevice(string newDevice)
+    private void EnterVR()
     {
-        if (String.Compare(XRSettings.loadedDeviceName, newDevice, true) != 0)
+        StartCoroutine(StartXR());
+        if (Api.HasNewDeviceParams())
         {
-            XRSettings.LoadDeviceByName(newDevice);
-            yield return null;
-            XRSettings.enabled = true;
+            Api.ReloadDeviceParams();
         }
+    }
+    private void ExitVR()
+    {
+        StopXR();
+    }
+
+    private IEnumerator StartXR()
+    {
+        Debug.Log("Initializing XR...");
+        yield return XRGeneralSettings.Instance.Manager.InitializeLoader();
+
+        if (XRGeneralSettings.Instance.Manager.activeLoader == null)
+        {
+            Debug.LogError("Initializing XR Failed.");
+        }
+        else
+        {
+            Debug.Log("XR initialized.");
+
+            Debug.Log("Starting XR...");
+            XRGeneralSettings.Instance.Manager.StartSubsystems();
+            Debug.Log("XR started.");
+        }
+    }
+
+    /// <summary>
+    /// Stops and deinitializes the Cardboard XR plugin.
+    /// See https://docs.unity3d.com/Packages/com.unity.xr.management@3.2/manual/index.html.
+    /// </summary>
+    private void StopXR()
+    {
+        Debug.Log("Stopping XR...");
+        XRGeneralSettings.Instance.Manager.StopSubsystems();
+        Debug.Log("XR stopped.");
+
+        Debug.Log("Deinitializing XR...");
+        XRGeneralSettings.Instance.Manager.DeinitializeLoader();
+        Debug.Log("XR deinitialized.");
     }
 }
